06ddc1b02a75a22f8bd4c2d9855334d6ea867d2c
kishorep-08 <kishore.pkdp@gmail.com>
2025-09-25 19:00:06 +0530

    updated

---
+++ 02-mongodb.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shell-script-logs"
+SCRIPT_NAME=$(echo "$0" | awk -F. '{print$1}')
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R Error $N: Install with root privileges" | tee -a $LOG_FILE
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ...... $R failed $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2......$G Success $N" | tee -a $LOG_FILE
+    fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+dnf install mongodb-org -y &>> $LOG_FILE
+VALIDATE $? "Adding mongo repo"
+
+systemctl enable mongod
+VALIDATE $? "Enable mongod"
+
+systemctl start mongod
+VALIDATE $? "Starting mongod"
