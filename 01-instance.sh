5332de5d265ceee274f836c299305958a9ae83b7
kishorep-08 <kishore.pkdp@gmail.com>
2025-09-25 18:30:05 +0530

    updated

--- 01-instance.sh
+++ 01-instance.sh
@@ -2,6 +2,8 @@
 
 AMI_ID=ami-09c813fb71547fc4f
 SG_ID=sg-0935c7424eff90287
+ZONE_ID=Z10420318WCX06C8HN1Q
+DOMAIN_NAME=kishore-p.space
 
 for instance in $@
 do
@@ -10,8 +12,31 @@ do
     if [ $instance != "frontend" ]
     then #Get Private IP
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[*].Instances[*].PrivateIpAddress" --output text)
+        RECORD_NAME=$instance.$DOMAIN_NAME
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[*].Instances[*].PublicIpAddress" --output text)
+        RECORD_NAME=$instance.$DOMAIN_NAME
     fi
+
     echo "$INSTANCE_ID : $IP"
+    
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record set"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }
+    '
 done
+
