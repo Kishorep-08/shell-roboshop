9c8a78b45a931a7f7767cf246d48da6d620ad596
kishorep-08 <kishore.pkdp@gmail.com>
2025-09-27 07:44:53 +0530

    MongoDB

--- 02-mongodb.sh
+++ 02-mongodb.sh
@@ -35,7 +35,7 @@ VALIDATE $? "Adding mongo repo"
 systemctl enable mongod &>> $LOG_FILE
 VALIDATE $? "Enable MongoDB"
 
-systemctl start mongod
+systemctl start mongod &>> $LOG_FILE
 VALIDATE $? "Starting MongoDB"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
