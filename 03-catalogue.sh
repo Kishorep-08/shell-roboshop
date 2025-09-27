e01a02573715133fb7dc149349d0506b0c02a8a4
kishorep-08 <kishore.pkdp@gmail.com>
2025-09-27 08:38:00 +0530

    Catalogue

--- 03-catalogue.sh
+++ 03-catalogue.sh
@@ -89,7 +89,7 @@ dnf install mongodb-mongosh -y &>> $LOG_FILE
 VALIDATE $? "Installing mongosh client"
 
 INDEX=$(mongosh mongodb.daws86s.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
-if [ $INDEX -le 0 ]; then
+if [ $INDEX -le 0 ];then
     mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Load catalogue products"
 else
