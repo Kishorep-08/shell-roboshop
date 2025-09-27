ef0790892f4b59b2417e9ddaa6a7df72b0047503
kishorep-08 <kishore.pkdp@gmail.com>
2025-09-27 08:21:40 +0530

    Catalogue

--- 03-catalogue.sh
+++ 03-catalogue.sh
@@ -43,7 +43,7 @@ VALIDATE $? "Installing nodejs"
 ################ Creating System user ################
 
 id roboshop &>> $LOG_FILE
-if [ $? -ne 0];then
+if [ $? -ne 0 ];then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOG_FILE
     VALIDATE $? "Creating System user"
 else
