4dae865562f21d3fed1ce3b3d5b9079c8e90aebe
kishorep-08 <kishore.pkdp@gmail.com>
2025-09-27 23:10:24 +0530

    update

---
+++ 11-frontend.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shell-script-logs"
+SCRIPT_NAME=$(echo "$0" | awk -F. '{print$1}')
+SCRIPT_DIR=$PWD
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+MYSQL_HOST=mysql.kishore-p.space
+START_TIME=$(date +%s)
+
+mkdir -p $LOGS_FOLDER
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R Error $N: Install with root privileges" | tee -a $LOG_FILE
+    exit 1
+fi
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$R Error $N: $2 got failed" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2......$G Success $N" | tee -a $LOG_FILE
+    fi
+}
+
+dnf install python3 gcc python3-devel -y &>> $LOG_FILE
+VALIDATE $? "Installing python"
+
+################ Creating System user ################
+
+id roboshop &>> $LOG_FILE
+if [ $? -ne 0 ];then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOG_FILE
+    VALIDATE $? "Creating System user"
+else
+    echo -e "User already exists ...... $Y Skipping $N" | tee -a $LOG_FILE
+fi
+
+######## Nginx Setup #########
+dnf module disable nginx -y &>> $LOG_FILE
+VALIDATE $? "Disabling default nginx"
+
+dnf module enable nginx:1.24 -y &>> $LOG_FILE
+VALIDATE $? "Enabling nginx:1.24"
+
+dnf install nginx -y &>> $LOG_FILE
+VALIDATE $? "Installing nginx"
+
+rm -rf /usr/share/nginx/html/* 
+
+############ Application code setup ############
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>> $LOG_FILE
+VALIDATE $? "Downloading code"
+
+cd /usr/share/nginx/html
+VALIDATE $? "Changing to nginx directory"
+
+unzip /tmp/frontend.zip &>> $LOG_FILE
+VALIDATE $? "Unzipping code"
+
+rm -rf /etc/nginx/nginx.conf
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
+VALIDATE $? "Copying nginx configuration"
+
+systemctl restart nginx 
