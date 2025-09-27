5f5cee21308e9b70c753d69b9bdb5a428c5ded2e
kishorep-08 <kishore.pkdp@gmail.com>
2025-09-27 08:22:26 +0530

    Catalogue

--- 03-catalogue.sh
+++ 03-catalogue.sh
@@ -88,5 +88,5 @@ VALIDATE $? "Creatinng mongo repo"
 dnf install mongodb-mongosh -y &>> $LOG_FILE
 VALIDATE $? "Installing mongosh client"
 
-mongosh --host $MONGODB_HOST </app/db/master-data.js
+mongosh --host $MONGODB_HOST </app/db/master-data.js &>> $LOG_FILE
 VALIDATE $? "Loading master data"
