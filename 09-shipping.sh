b79295da033ea6e1af0498fb56d22e343c71bade
kishorep-08 <kishore.pkdp@gmail.com>
2025-09-27 22:04:08 +0530

    cart

---
+++ 09-shipping.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shell-script-logs"
+SCRIPT_NAME=$(echo "$0" | awk -F. '{print$1}')
+SCRIPT_DIR=$PWD
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+MYSQL_HOST=mysql.kishore-p.space
+START_TIME=$(date +%s)
+
+mkdir -p $LOGS_FOLDER
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R Error $N: Install with root privileges" | tee -a $LOG_FILE
+    exit 1
+fi
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$R Error $N: $2 got failed" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2......$G Success $N" | tee -a $LOG_FILE
+    fi
+}
+
+dnf install maven -y &>> $LOG_FILE
+VALIDATE $? "Installing Maven"
+
+################ Creating System user ################
+
+id roboshop &>> $LOG_FILE
+if [ $? -ne 0 ];then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOG_FILE
+    VALIDATE $? "Creating System user"
+else
+    echo -e "User already exists ...... $Y Skipping $N" | tee -a $LOG_FILE
+fi
+
+mkdir -p /app 
+VALIDATE $? "Creating app directory"
+
+############ Application code setup ############
+curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip &>> $LOG_FILE
+VALIDATE $? "Downloading code"
+
+cd /app 
+VALIDATE $? "Changing to app directory"
+
+rm -rf /app/*
+VALIDATE $? "Removing existing code"
+
+unzip  /tmp/cart.zip &>> $LOG_FILE
+VALIDATE $? "unzipping code"
+
+mvn clean package &>> $LOG_FILE
+VALIDATE $? "Installing dependencies and building artifacts"
+
+mv target/shipping-1.0.jar shipping.jar
+VALIDATE $? "Moving .jar file into app directory"
+
+cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service
+VALIDATE $? "Creating shipping service "
+
+systemctl daemon-reload
+
+systemctl enable cart &>> $LOG_FILE
+VALIDATE $? "Enabling shipping service"
+
+systemctl start cart &>> $LOG_FILE
+VALIDATE $? "Starting shipping service"
+
+dnf install mysql -y &>> $LOG_FILE
+VALIDATE $? "Installing mysql client"
+
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities'
+if [ $? -ne 0 ]; then
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql 
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql
+else
+    echo -e "Schema already exists ...... $Y Skipping $N"
+
+systemctl restart shipping
+
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME - $START_TIME))
+
+echo -e  "Script Executed in $Y $TOTAL_TIME seconds $N"
